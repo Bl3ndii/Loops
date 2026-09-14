611a9331e2f61caa3b6e049f5d1b7da671be736e
Blend <120633155+Bl3ndii@users.noreply.github.com>
2026-09-14 20:07:30 +0000

    Loops fertig

---
+++ loops.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    //for(i = 1; i <= 5; ++i)
+    //for(i = 0; i < 7; i += 1.5)
+    //for(i = 20; i >  5; i -= 5)
+    //for(i = 1; i <= 20; i *= 2)
+    //     printf("%d  ", i);
+
+    //return 0;
+
+    for(i = 1; i < 5; i++)
+    {
+        printf("\nEine Schleife: ");
+        printf("%d ", i);
+    }
+
+}
